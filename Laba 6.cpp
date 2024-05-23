4ed10180433a02812d77c3bd58663e493a686350
alexblin123 <130595593+alexblin123@users.noreply.github.com>
2024-05-23 19:22:03 +0500

    Add files via upload

---
+++ Laba 6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    system("chcp 1251 > Null");
+
+    string str;
+
+    cout << "Введите предложение: "; getline(cin, str);
+    int m = str.size();
+
+    for (int i = 0; i < m / 2; i++) {
+        for (int k = 0; k < i; k++) {
+            cout << " ";
+        }
+        cout << str[i];
+        for (int j = i; j < m - i; j++) {
+            cout << " ";
+        }
+        cout << str[m - i - 1] << endl;
+    }
+    return 0;
+}
